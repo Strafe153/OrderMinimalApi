e937e96596ca9de603490205db179bb3d16d710b
Vitalik Knysh <knish.vital@gmail.com>
2022-09-27 22:42:21 +0300

    Added CancellationTokens to Get requests

--- Repositories/IOrderRepository.cs
+++ Repositories/IOrderRepository.cs
@@ -4,8 +4,8 @@ namespace OrderMinimalApi.Repositories;
 
 public interface IOrderRepository
 {
-    Task<IEnumerable<Order>> GetAllAsync();
-    Task<Order?> GetByIdAsync(string id);
+    Task<IEnumerable<Order>> GetAllAsync(CancellationToken token = default);
+    Task<Order?> GetByIdAsync(string id, CancellationToken token = default);
     Task CreateAsync(Order order);
     Task UpdateAsync(string id, Order newOrder);
     Task DeleteAsync(string id);
