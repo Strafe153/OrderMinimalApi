bb726af52aafbcc143e9154c8fa7cde1415c3c65
Vitalik Knysh <knish.vital@gmail.com>
2023-06-29 00:10:57 +0300

    Fixed the indentation

--- Configurations/ApiVersioningConfiguration.cs
+++ Configurations/ApiVersioningConfiguration.cs
@@ -7,13 +7,14 @@ public static class ApiVersioningConfiguration
 {
     public static void ConfigureApiVersioning(this IServiceCollection services)
     {
-        services.AddApiVersioning(options =>
-        {
-            options.AssumeDefaultVersionWhenUnspecified = true;
-            options.DefaultApiVersion = new ApiVersion(1, 0);
-            options.ReportApiVersions = true;
-            options.ApiVersionReader = new UrlSegmentApiVersionReader();
-        })
+        services
+            .AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            })
             .AddVersionedApiExplorer(options =>
             {
                 options.GroupNameFormat = "'v'VVV";
