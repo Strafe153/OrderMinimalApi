587f63dbc35afa5b64b5fb8b08cfb949e7c418c9
Vitalik Knysh <knish.vital@gmail.com>
2024-09-01 20:40:48 +0300

    Removed unused constants

--- Domain/Shared/Constants/CacheConstants.cs
+++ Domain/Shared/Constants/CacheConstants.cs
@@ -4,7 +4,4 @@ public static class CacheConstants
 {
 	public const string OrdersTag = "orders";
 	public const string OrderTag = "order";
-
-	public const string UsersTag = "users";
-	public const string UserTag = "user";
 }
