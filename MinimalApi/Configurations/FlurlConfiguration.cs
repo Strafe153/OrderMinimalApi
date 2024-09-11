6a404df1344b86d7525a22c3d4ca3efa3456d39c
Vitalik Knysh <knish.vital@gmail.com>
2024-09-11 23:47:07 +0300

    Added flurl instead of restsharp

---
+++ MinimalApi/Configurations/FlurlConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Shared.Constants;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+using MinimalApi.Configurations.Models;
+
+namespace MinimalApi.Configurations;
+
+public static class FlurlConfiguration
+{
+	public static void ConfigureFlurl(this IServiceCollection services, IConfiguration configuration)
+	{
+        var seqOptions = configuration.GetSection(SeqOptions.SectionName).Get<SeqOptions>()!;
+
+        services.AddSingleton(p =>
+            new FlurlClientCache()
+                .Add(FlurlConstants.SeqClient, seqOptions.ConnectionString, b => b.WithTimeout(TimeSpan.FromSeconds(.1))));
+    }
+}
--- MinimalApi/Policies/PolicyProvider.cs
+++ MinimalApi/Policies/PolicyProvider.cs
@@ -1,14 +1,13 @@
-﻿using Polly;
+﻿using Flurl.Http;
+using Polly;
 using Polly.Retry;
-using RestSharp;
 
 namespace MinimalApi.Policies;
 
 public static class PolicyProvider
 {
-    public static AsyncRetryPolicy<RestResponse> WaitAndRetryPolicy => Policy
-        .HandleResult<RestResponse>(r => !r.IsSuccessStatusCode)
-        .Or<HttpRequestException>()
-        .Or<TimeoutException>()
-        .WaitAndRetryAsync(3, retry => TimeSpan.FromSeconds(Math.Pow(1.5, retry)));
+    public static AsyncRetryPolicy FlurlWaitAndRetryPolicy =>
+        Policy
+            .Handle<FlurlHttpException>()
+            .WaitAndRetryAsync(3, retry => TimeSpan.FromSeconds(Math.Pow(1.5, retry)));
 }
